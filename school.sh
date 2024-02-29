e76459c51d47696ff93ac74222fd1c5104c7e1a2
Karan Kahlon <kkahlon@wisc.edu>
2024-02-29 12:10:04 -0600

    Completed step 4 school.sh script.

--- school.sh
+++ school.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d , -f 7 | { sum=0; numVals=0;  while read n; do sum=$(($sum + $n)); numVals=$(($numVals + 1)); done; bc -l <<< "$sum / $numVals";}
