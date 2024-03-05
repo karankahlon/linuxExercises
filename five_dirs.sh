fc114bcb5b9292bfb3dc6271022860c3680a1923
June <lee2275@wisc.edu>
2024-03-05 11:49:10 -0600

    finished five directories

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+mkdir five 
+x=1
+
+while [[ $x -lt 6 ]]
+do
+    mkdir five/dir$x
+    y=1
+
+    while [[ $y -lt 5 ]]
+    do
+	
+	z=1
+
+	while [[ $z -lt $y+1 ]]
+	do
+	    
+	    echo $y >> five/dir$x/file$y
+	    z=$(($z+1))
+	    
+	done
+       
+        y=$(($y+1))
+    done
+
+    x=$(($x+1))
+done
