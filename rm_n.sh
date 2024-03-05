321a20296206889a449f295870539663559a453e
Yi Yu <yu353@wisc.edu>
2024-03-05 12:10:26 -0600

    add the rm_n.sh

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+usage() {
+  echo "Usage: $0 <dir> <n>" 1>&2
+}
+
+if [ "$#" -ne 2 ]; then
+  usage
+  exit 1
+fi
+
+dir=$1
+n=$2
+
+if [ ! -d "$dir" ]; then
+  echo "Error: $dir is not a valid directory" 1>&2
+  exit 2
+fi
+
+find "$dir" -type f -size +"${n}c" -exec rm {} +
+
